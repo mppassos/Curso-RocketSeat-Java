66d4cbdd3e43de2625d086510fd9c90e1c8937a5
Matheus Oliveira <mdosolive@proton.me>
2023-10-15 09:37:16 -0300

    Curso Rocket-Seat - Java

---
+++ src/main/java/br/com/matheusoliveira/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,25 @@
+/*package br.com.matheusoliveira.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+// http://localhost:8080/primeiraRota - - - -
+public class MinhaPrimeiraController {
+
+    /**
+     * METODOS DE ACESSO AO HTTP
+     * GET - Buscar uma informação
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar um dado/informação
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte do dado / info
+     */
+    /*@GetMapping("/primeiroMetodo")
+    public String primeiraMensagem(){
+        return "Funcionou";
+    }
+    
+}*/
---
+++ src/test/java/br/com/matheusoliveira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusoliveira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
